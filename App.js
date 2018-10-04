41ac963673db420726636a053431d59215049528
Mikel Garcia <mikel_6@hotmail.com>
2018-10-04 12:01:24 +0200

    Add Box to GameEngine entities

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import React from "react";
-import { StatusBar, StyleSheet } from "react-native";
+import { Dimensions, StatusBar, StyleSheet } from "react-native";
 import Matter from "matter-js";
 import { GameEngine } from "react-native-game-engine";
+
+import Box from "./Box";
+
+const { width, height } = Dimensions.get("screen");
+const boxSize = Math.trunc(Math.max(width, height) * 0.075);
+const initialBox = Matter.Bodies.rectangle(
+  width / 2,
+  height / 2,
+  boxSize,
+  boxSize
+);
 
 export default class App extends React.Component {
   render() {
     return (
-      <GameEngine style={styles.container}>
+      <GameEngine
+        style={styles.container}
+        entities={{
+          initialBox: {
+            body: initialBox,
+            size: [boxSize, boxSize],
+            color: "red",
+            renderer: Box
+          }
+        }}
+      >
         <StatusBar hidden={true} />
       </GameEngine>
     );
